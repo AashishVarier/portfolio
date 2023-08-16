2c75b1421ebc9597600a01ca5215cef939d0457f
arvariable <35440657+AashishVarier@users.noreply.github.com>
2023-08-16 23:51:16 +0530

    added findme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # portfolio-site
 portfolio website using HTML, CSS, JavaScript 
 
+Site is live at https://aashishvarier.github.io/portfolio/
---
+++ script/script.js
@@ -0,0 +1,4 @@
+//scroll to bottom
+document.getElementById("scroll-to-bottom").addEventListener("click", function () {
+    document.body.scrollIntoView(false);
+  });
